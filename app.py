983e03b01e63aae7fd3f2099946a54d85cd7a756
142178KZ <kazu.aviles@gmail.com>
2024-03-19 00:10:11 -0300

    agregar grafico dispersión test

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import streamlit as st
         
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
+
     
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
@@ -14,3 +15,20 @@ if hist_button: # al hacer clic en el botón
     
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+    # crear segundo botton
+
+dis_button = st.button('Construir gráfico dispersión') # crear un botón
+
+if dis_button: # al hacer clic en el botón
+    # escribir encabezado
+    st.header('Modelos de autos') 
+    
+    # escribir un mensaje
+    st.write('Creación de un gráfico de dispersión para los modelos por año de coches')
+
+    # crear un gráfico de dispersión
+    fig_dis = px.scatter(car_data, x="model_year", y="price") 
+
+    # mostrar un gráfico de dispersión Plotly interactivo
+    st.plotly_chart(fig_dis, use_container_width=True)
